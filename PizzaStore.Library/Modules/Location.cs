247728235d118fafd59e2c19e3025b450833b964
joeisble <joeisble@yahoo.com>
2018-07-01 15:26:52 -0400

    get Library, Test, and UI project set up

---
+++ PizzaStore.Library/Modules/Location.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaStore.Library
+{
+    public class Location
+    {
+        public DateTime OrderHistory { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
---
+++ PizzaStore.Library/Modules/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaStore.Library
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Location { get; set; }
+        public string OrderPref { get; set; }
+    }
+}
