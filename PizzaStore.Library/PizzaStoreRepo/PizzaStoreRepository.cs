c4d38a137b303b722ef38b4e845d2bf8b9c24d85
joeisble <joeisble@yahoo.com>
2018-07-11 13:59:17 -0400

    pizzastore repo class was in wrong location

--- PizzaStore.Library/PizzaStoreRepo/PizzaStoreRepository.cs
+++ PizzaStore.Library/PizzaStoreRepo/PizzaStoreRepository.cs
@@ -35,26 +35,110 @@ namespace PizzaStore.Library.PizzaStoreRepo
             _db.Add(Mapper.Map(user));
         }
 
-        public void DeleteUserFromDB(User userID)
+        public bool IsLocationInDB(int loc)
         {
-            _db.Remove(_db.Users.Find(userID.ID));
+            var locations = _db.Locations;
+            foreach (var item in locations)
+            {
+                if (loc == item.StoreNumber)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
-        public void UpdateLocationInventory(Location inventory)
+        public int GetUserLocation (string first, string last)
         {
-            _db.Entry(_db.Locations.Find(inventory.StoreNumber)).CurrentValues.SetValues(Mapper.Map(inventory));
+            var users = _db.Users;
+            foreach (var item in users)
+            {
+                if (first == item.FirstName && last == item.LastName)
+                {
+                    return item.PrefLocation;
+                }
+            }
+            return 0;
+        }
+        public bool IsUserInDB(string first, string last)
+        {
+            var users = _db.Users;
+            foreach (var item in users)
+            {
+                if (first == item.FirstName && last == item.LastName)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
-        public IEnumerable<Order> GetOrderHistory()
+        public User GetUser(string first, string last, int storenumber)
         {
-            var OrderList = Mapper.Map(_db.Orders.Include(x => x.StoreNumber).Include(y => y.UserId).AsNoTracking());
-
-            return OrderList;
+            var users = _db.Users;
+            foreach (var item in users)
+            {
+                if (first == item.FirstName && last == item.LastName && storenumber == item.PrefLocation)
+                {
+                    return Mapper.Map(item);
+                }
+            }
+            return new User(first,last,storenumber);
         }
 
-        public IEnumerable<User> GetUsers()
+        public int GetUserID(User user)
         {
-            return Mapper.Map(_db.Users.Include(u => u.Orders).AsNoTracking());
+            var users = _db.Users;
+            foreach (var item in users)
+            {
+                if (user.First == item.FirstName && user.Last == item.LastName)
+                {
+                    return item.Id;
+                }
+            }
+            return -1;
+        }
+
+        public bool DoesUserHavePreviousOrders(User user)
+        {
+            var orders = _db.Orders;
+            foreach (var item in orders)
+            {
+                if (user.ID == item.UserId)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public List<Order> GetUserOrderHistory(User user)
+        {
+            var orders = _db.Orders;
+            List<Order> ListOfOrders = new List<Order>();
+            foreach (var item in orders)
+            {
+                if (user.ID == item.UserId)
+                {
+                    ListOfOrders.Add(Mapper.Map(item));
+                }
+            }
+            return ListOfOrders;
+        }
+        public Order GetUserOrders(User user)
+        {
+            var orders = _db.Orders;
+            Order lastitem = new Order();
+
+
+            foreach (var item in orders)
+            {
+                if (user.ID == item.UserId)
+                {
+                    lastitem =  Mapper.Map(item);
+                }
+            }
+            return lastitem;
         }
 
         public void Save()
